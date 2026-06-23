1b703b28aad9674b69b9792aee82a0f2e02d793c
unknown <bhsairupesh@gmail.com>
2026-06-23 16:00:44 +0530

    changes

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,17 @@
+importScripts("https://www.gstatic.com/firebasejs/10.7.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.7.0/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: ProcessingInstruction.env.api_key,
+  authDomain: "smartpillbox-69f36.firebaseapp.com",
+  projectId: "smartpillbox-69f36"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body,
+    icon: "/icon-192.png"
+  });
+});
